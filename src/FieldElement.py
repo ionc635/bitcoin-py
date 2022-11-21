ed6afe9f9762fce2414c175c8c399c4180975d62
steve <ionc635@naver.com>
2022-11-21 23:14:22 +0900

    test: 테스트 코드 상수 처리

--- src/FieldElement.py
+++ src/FieldElement.py
@@ -57,38 +57,42 @@ class FieldElement:
 
 class FieldElementTest(unittest.TestCase):
     def test_add(self):
-        a = FieldElement(44, 57)
-        b = FieldElement(33, 57)
-        self.assertEqual(a + b, FieldElement(20, 57))
-        c = FieldElement(17, 57)
-        d = FieldElement(42, 57)
-        e = FieldElement(49, 57)
-        self.assertEqual(c + d + e, FieldElement(51, 57))
+        p = 57
+        a = FieldElement(44, p)
+        b = FieldElement(33, p)
+        self.assertEqual(a + b, FieldElement(20, p))
+        c = FieldElement(17, p)
+        d = FieldElement(42, p)
+        e = FieldElement(49, p)
+        self.assertEqual(c + d + e, FieldElement(51, p))
 
     def test_sub(self):
-        a = FieldElement(9, 57)
-        b = FieldElement(29, 57)
-        self.assertEqual(a - b, FieldElement(37, 57))
-        c = FieldElement(52, 57)
-        d = FieldElement(30, 57)
-        e = FieldElement(38, 57)
-        self.assertEqual(c - d - e, FieldElement(41, 57))
+        p = 57
+        a = FieldElement(9, p)
+        b = FieldElement(29, p)
+        self.assertEqual(a - b, FieldElement(37, p))
+        c = FieldElement(52, p)
+        d = FieldElement(30, p)
+        e = FieldElement(38, p)
+        self.assertEqual(c - d - e, FieldElement(41, p))
 
     def test_mul(self):
-        a = FieldElement(95, 97)
-        b = FieldElement(45, 97)
-        c = FieldElement(31, 97)
-        self.assertEqual(a * b * c, FieldElement(23, 97))
-        d = FieldElement(17, 97)
-        e = FieldElement(13, 97)
-        f = FieldElement(19, 97)
-        g = FieldElement(44, 97)
-        self.assertEqual(d * e * f * g, FieldElement(68, 97))
+        p = 97
+        a = FieldElement(95, p)
+        b = FieldElement(45, p)
+        c = FieldElement(31, p)
+        self.assertEqual(a * b * c, FieldElement(23, p))
+        d = FieldElement(17, p)
+        e = FieldElement(13, p)
+        f = FieldElement(19, p)
+        g = FieldElement(44, p)
+        self.assertEqual(d * e * f * g, FieldElement(68, p))
 
     def test_truediv(self):
-        a = FieldElement(3, 31)
-        b = FieldElement(24, 31)
-        self.assertEqual(a / b, FieldElement(4, 31))
-        c = FieldElement(4, 31)
-        d = FieldElement(11, 31)
-        self.assertEqual(a ** -4 * d, FieldElement(12, 31))
+        p = 31
+        a = FieldElement(3, p)
+        b = FieldElement(24, p)
+        self.assertEqual(a / b, FieldElement(4, p))
+        c = FieldElement(4, p)
+        d = FieldElement(11, p)
+        self.assertEqual(a ** -4 * d, FieldElement(12, p))
