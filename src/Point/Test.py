d2a07cb29d7f4a247a9c36f38f444c1797be59a7
steve <ionc635@naver.com>
2022-12-02 23:07:18 +0900

    add: 유한체 점 덧셈 테스트 코드 추가

--- src/Point/Test.py
+++ src/Point/Test.py
@@ -32,3 +32,28 @@ class PointTest(unittest.TestCase):
             y = FieldElement(y_raw, prime)
             with self.assertRaises(ValueError):
                 Point(x, y, a, b)
+
+    def test_FieldElement_Point_add(self):
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+
+        additions = (
+            (192, 105, 17, 56, 170, 142),
+            (47, 71, 117, 141, 60, 139),
+            (143, 98, 76, 66, 47, 71),
+        )
+
+        for x1_num, y1_num, x2_num, y2_num, x3_num, y3_num in additions:
+            x1 = FieldElement(x1_num, prime)
+            y1 = FieldElement(y1_num, prime)
+            x2 = FieldElement(x2_num, prime)
+            y2 = FieldElement(y2_num, prime)
+            x3 = FieldElement(x3_num, prime)
+            y3 = FieldElement(y3_num, prime)
+
+            p1 = Point(x1, y1, a, b)
+            p2 = Point(x2, y2, a, b)
+            p3 = Point(x3, y3, a, b)
+
+            self.assertEqual(p1 + p2, p3)
