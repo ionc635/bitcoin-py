14f3db086c7f6d6b50f57a30c7470dd95e20c7a5
steve <ionc635@naver.com>
2022-12-02 22:18:54 +0900

    chore: 테스트 코드 파일 생성

--- src/Point/Point.py
+++ src/Point/Point.py
@@ -54,34 +54,3 @@ class Point:
             x = s ** 2 - 2 * self.x
             y = s * (self.x - x) - self.y
             return self.__class__(x, y, self.a, self.b)
-
-class PointTest(unittest.TestCase):
-    def test_point_add(self):
-        a = 5
-        b = 7
-        p1 = Point(2, 5, a, b)
-        p2 = Point(-1, -1, a, b)
-        self.assertEqual(p1 + p2, Point(3, -7, a, b))
-
-    def test_point_add1(self):
-        a = 5
-        b = 7
-        p1 = Point(-1, -1, a, b)
-        p2 = Point(-1, -1, a, b)
-        self.assertEqual(p1 + p2, Point(18, 77, a, b))
-
-    def test_on_curve(self):
-        prime = 223
-        a = FieldElement(0, prime)
-        b = FieldElement(7, prime)
-        valid_points = ((192, 105), (17, 56), (1, 193))
-        invalid_points = ((200, 119), (42, 99))
-        for x_raw, y_raw in valid_points:
-            x = FieldElement(x_raw, prime)
-            y = FieldElement(y_raw, prime)
-            Point(x, y, a, b)
-        for x_raw, y_raw in invalid_points:
-            x = FieldElement(x_raw, prime)
-            y = FieldElement(y_raw, prime)
-            with self.assertRaises(ValueError):
-                Point(x, y, a, b)
