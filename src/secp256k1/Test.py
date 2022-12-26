edcf74cbb5789e4a796fd1642639f79bcaf4b5e4
steve <ionc635@naver.com>
2022-12-26 22:52:06 +0900

    add: DER 서명 형식 구현현

--- src/secp256k1/Test.py
+++ src/secp256k1/Test.py
@@ -48,4 +48,9 @@ class secp256k1(unittest.TestCase):
         print(pk5.point.sec(True))
         print(pk6.point.sec(True))
 
+    def test_der(self):
+        r = 0x37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6
+        s = 0x8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec
 
+        sig = Signature(r, s)
+        print('test_der', sig.der())
