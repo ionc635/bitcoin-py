0efd591e33a0a3f9d9ef958e2a195ae39edf7086
steve <ionc635@naver.com>
2023-01-10 00:24:21 +0900

    add: Tx Class 구현

---
+++ src/Tx/Tx.py
@@ -0,0 +1,80 @@
+from ..helper.helper import (
+    hash256,
+    encode_varint,
+    int_to_little_endian,
+    little_endian_to_int,
+    read_varint
+)
+
+class Tx:
+    def __init__(self, version, tx_ins, tx_outs, locktime, testnet = False)
+        self.version = version
+        self.tx_ins = tx_ins
+        self.tx_outs = tx_outs
+        self.locktime = locktime
+        self.testnet = testnet
+    
+    def __repr__(self):
+        tx_ins = ''
+        for tx_in in self.tx_ins:
+            tx_ins += tx_in.__repr__() + '\n'
+        tx_outs = ''
+        for tx_out in self.tx_outs:
+            tx_outs += tx_out.__repr__() + '\n'
+        return 'tx: {}\nversion: {}\ntx_ins:\n{}tx_outs:\n{}locktime: {}'.format(
+            self.id(),
+            self.version,
+            tx_ins,
+            tx_out,
+            self.locktime,
+        )
+    
+    def id(self):
+        '''Human-readable hexadecimal of the transaction hash'''
+        return self.hash().hex()
+
+    def hash(self):
+        '''Bianry hash of the legacy serialization'''
+        return hash256(self.serialize())[::-1]
+
+    def serialize(self):
+        '''Return the byte serialization of the transaction'''
+        # serialize version (4 bytes, little endian)
+        result = int_to_little_endian(self.version, 4)
+        # encode_varint on the number of inputs
+        result += encode_varint(len(self.tx_ins))
+        # iterate inputs
+        for tx_in in self.tx_ins:
+            # serialize each input
+            result += tx_in.serialize()
+        # encode_varint on the number of outputs
+        result += encode_varint(len(self.tx_outs))
+        # iterate outputs
+        for tx_out in self.tx_outs:
+            # serialize each output
+            result += tx_out.serialize()
+        # serialize locktime (4 bytes, little endian)
+        result += int_to_little_endian(self.locktime, 4)
+        return result
+
+    @classmethod
+    def parse(cls, s, testnet = False):
+        '''Takes a byte stream and parses the transaction at the start return a TX object'''
+        # s.read(n) will return n bytes
+        # version is an integer in 4 bytes, little-endian
+        version = little_endian_to_int(s.read(4))
+        # num_inputs is varint, use read_varint(s)
+        num_inputs = read_varint(s)
+        # parse num_inputs number of TxIns
+        inputs = []
+        for _ in range(num_inputs):
+            inputs.append(TxIn.parse(s))
+        # num_ouputs is a varint, use read_varint(s)
+        outputs = []
+        for _ in range(num_outputs):
+            outputs.append(TxOut.parse(s))
+        # locktime is an integer in 4 bytes, little-endian
+        locktime = little_endian_to_int(s.read(4))
+        return cls(version, inputs, outputs, locktime, testnet = testnet)
+
+
