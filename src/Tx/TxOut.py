8ea94a9d9e4a71e947dcb32d0a65c19faf48270a
steve <ionc635@naver.com>
2023-03-03 00:46:46 +0900

    add: Implement Transaction Output
    - init, repr

---
+++ src/Tx/TxOut.py
@@ -0,0 +1,7 @@
+class TxOut:
+    def __init__(self, amount, script_pubkey):
+        self.amount = amount
+        self.script_pubkey = script_pubkey
+
+    def __repr__(self):
+        return '{}:{}'.format(self.amount, self.script_pubkey)
