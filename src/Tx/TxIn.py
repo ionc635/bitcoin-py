407a4404c115e80f28a897081d0ba2e6e489679c
steve <ionc635@naver.com>
2023-03-03 00:43:12 +0900

    add: Implement Transaction Input
    - init, repr function

---
+++ src/Tx/TxIn.py
@@ -0,0 +1,15 @@
+class TxIn:
+    def __init__(self, prev_tx, prev_index, script_sig = None, sequence = 0xffffffff):
+        self.prev_tx = prev_tx
+        self.prev_index = prev_index
+        if script_sig is None:
+            self.script_sig = Script()
+        else:
+            self.script_sig = script_sig
+        self.sequence = sequence
+
+    def __repr__(self):
+        return '{}:{}'.format(
+            self.prev_tx.hex(),
+            self.prev_index,
+        )
