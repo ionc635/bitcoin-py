0e7fbcf1f9dea6bb073d1961c8d80223c71443a8
steve <ionc635@naver.com>
2023-03-29 22:09:06 +0900

    add: Implement TxIn, TxOut parse, serialize

--- src/Tx/TxIn.py
+++ src/Tx/TxIn.py
@@ -1,4 +1,5 @@
 from ..Script import Script
+from ..helper import int_to_little_endian
 
 class TxIn:
     def __init__(self, prev_tx, prev_index, script_sig = None, sequence = 0xffffffff):
@@ -23,3 +24,10 @@ class TxIn:
         script_sig = Script.parse()
         sequence = little_endian_to_int(s.read(4))
         return cls(prev_tx, prev_index, script_sig, sequence)
+
+    def serialize(self):
+        result = self.prev_tx[::-1]
+        result += int_to_little_endian(self.prev_index, 4)
+        result += self.script_sig.serialize()
+        result += int_to_little_endian(self.sequence, 4)
+        return result
--- src/Tx/TxOut.py
+++ src/Tx/TxOut.py
@@ -1,7 +1,19 @@
+from ..helper import int_to_little_endian
+from ..Script import Script
+
 class TxOut:
     def __init__(self, amount, script_pubkey):
         self.amount = amount
-        self.script_pubkey = script_pubkey
+        self.script_pubkey = Script.parse()
 
     def __repr__(self):
         return '{}:{}'.format(self.amount, self.script_pubkey)
+
+    def parse(cls, s):
+        amount = little_endian_to_int(s.read(8))
+        script_pubkey = Script.parse()
+
+    def serialize(self):
+        result = int_to_little_endian(self.amount(8))
+        result += self.script_pubkey.serialize()
+        return result
